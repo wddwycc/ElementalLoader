d25ff8a54c20ede67c215e056f2eb4fb98a19bd7
Carl <wddwyss@gmail.com>
2015-08-13 11:14:43 +0800

    Initial Commit

---
+++ ElementalLoader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ElementalLoader
+//
+//  Created by Carrl on 15/8/13.
+//  Copyright (c) 2015年 monk-studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
